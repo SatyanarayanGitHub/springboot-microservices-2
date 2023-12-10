fb2256d932ddc3c3f960fa6206a94732d94c1f40
Satyanarayan <satyanarayan.y@gmail.com>
2023-12-10 23:22:01 +0530

    Service Registry

---
+++ service-registry-master/src/test/java/com/srysoft/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srysoft.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
