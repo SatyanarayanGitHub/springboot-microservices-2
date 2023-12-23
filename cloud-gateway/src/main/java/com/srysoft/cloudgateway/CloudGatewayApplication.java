0596fcd1ce34d852fcbc3dcdf1c80b105433c5e3
Satyanarayan <satyanarayan.y@gmail.com>
2023-12-23 20:19:53 +0530

    API Gateway fixed

--- cloud-gateway/src/main/java/com/srysoft/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/srysoft/cloudgateway/CloudGatewayApplication.java
@@ -2,16 +2,12 @@ package com.srysoft.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableEurekaClient
-@EnableHystrix
+
 @SpringBootApplication
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
 }
